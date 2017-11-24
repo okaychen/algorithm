44590a2abd3c8aeb8c09e95aaad252e3929d7a63
okaychen <1440281580@qq.com>
2017-11-24 18:44:40 +0800

    add chainStack.cpp

---
+++ chainStack.cpp
@@ -0,0 +1,59 @@
+/***链栈实现数制的转换***/
+#include<iostream>
+using namespace std;
+
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+typedef int Status;
+typedef struct SNode{
+	int data;
+	struct SNode *next;
+}SNode,*LinkStack;
+
+Status InitStack(LinkStack &S)
+{
+	S = NULL;
+	return OK;
+}
+bool StackEmpty(LinkStack S)
+{
+	if(!S)
+		return true;
+	return false;
+}
+Status Push(LinkStack &S,int e)
+{
+	SNode *p = new SNode;
+	if(!p)
+	{
+		return OVERFLOW;
+	}
+	p->data = e;
+	p->next = S;
+	S = p;
+	return OK;
+}
+Status Pop(LinkStack &S,int &e)
+{
+	SNode *p;
+	if(!S)
+		return ERROR;
+	e = S->data;
+	p = S;
+	S = S->next;
+	delete p;
+	return OK;
+}
+
+//算法3.8　数制的转换(链栈实现)
+void conversion ( ) {
+   //对于输入的任意一个非负十进制数，打印输出与其等值的八进制数
+
+}
+int main()
+{
+	conversion();
+	return 0;
+}
+
